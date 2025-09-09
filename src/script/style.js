91013446bd7fb82f675811779bc85cf4b4b608e3
puthsabbosngang <puthsabbos.ngang21@gmail.com>
2025-09-09 17:36:13 +0700

    update screen

--- src/script/style.js
+++ src/script/style.js
@@ -1,7 +1,9 @@
-const calculaterMenu = document.getElementById("calculaterMenuPanel");
+const toggleContainer = document.getElementById("toggleContainerPanel")
+const calculatorMenu = document.getElementById("calculatorMenuPanel");
 const toggleCalculatorBtn = document.querySelector(".toggle-calculator-menu");
 function toggleCalculatorMenu() {
-    calculaterMenuPanel.style.display = (calculaterMenuPanel.style.display === "block") ? "none" : "block";
+    toggleContainer.style.display = (toggleContainer.style.display === "block") ? "none" : "block";
+    calculatorMenu.style.display = (calculatorMenu.style.display === "block") ? "none" : "block";
 }
 toggleCalculatorBtn.addEventListener("click", toggleCalculatorMenu);
 
@@ -9,7 +11,8 @@ toggleCalculatorBtn.addEventListener("click", toggleCalculatorMenu);
 const historyMenu = document.getElementById("historyPanel");
 const toggleHistoryBtn = document.querySelector(".toggle-history");
 function toggleHistory() {
-    historyPanel.style.display = (historyPanel.style.display === "block") ? "none" : "block";
+    toggleContainer.style.display = (toggleContainer.style.display === "block") ? "none" : "block";
+    historyMenu.style.display = (historyMenu.style.display === "block") ? "none" : "block";
 }
 toggleHistoryBtn.addEventListener("click", toggleHistory);
 
@@ -17,19 +20,21 @@ toggleHistoryBtn.addEventListener("click", toggleHistory);
 const memoryMenu = document.getElementById("memoryPanel");
 const toggleMemoryBtn = document.querySelector(".toggle-memory");
 function toggleMemory() {
-    memoryPanel.style.display = (memoryPanel.style.display === "block") ? "none" : "block";
+    toggleContainer.style.display = (toggleContainer.style.display === "block") ? "none" : "block";
+    memoryMenu.style.display = (memoryMenu.style.display === "block") ? "none" : "block";
 }
 toggleMemoryBtn.addEventListener("click", toggleMemory);
 
 
-document.querySelector('.calculator').addEventListener('click', function (e) {
+document.querySelector('.toggle-container').addEventListener('click', function (e) {
     if (
         e.target.closest('.toggle-calculator-menu') ||
         e.target.closest('.toggle-history') ||
         e.target.closest('.toggle-memory')
     ) return;
 
-    calculaterMenuPanel.style.display = "none";
-    historyPanel.style.display = "none";
-    memoryPanel.style.display = "none";
+    calculatorMenu.style.display = "none";
+    historyMenu.style.display = "none";
+    memoryMenu.style.display = "none";
+    toggleContainer.style.display = "none";
 });
